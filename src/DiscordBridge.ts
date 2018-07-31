2f1ed150b92f87b21ab1e384094cedac5d16ace7
MatthewSH <matthewh@halfpetal.com>
2018-07-31 17:01:08 -0400

    Adding Discord to StreamMe bridge.

--- src/DiscordBridge.ts
+++ src/DiscordBridge.ts
@@ -1,1 +1,30 @@
-// Placeholder.
+import * as Discord from "discord.js";
+
+let api;
+let helper;
+let discordClient;
+
+exports.constructor = (api: any, helper: any) => {
+    this.api = api;
+    this.helper = helper;
+    this.discordClient = new Discord.Client();
+
+    this.discordClient.on("ready", () => {
+        console.log("[DiscordBridge] Discord client ready.");
+    });
+
+    this.discordClient.on("message", (message: any) => {
+        if (message.channel.name.toLowerCase() === process.env.DISCORDBRIDGE_DISCORD_CHANNEL) {
+            this.api.say(message.content + " [" + message.author.username + "#" + message.author.discriminator + "]");
+        }
+    });
+
+    this.discordClient.login(process.env.DISCORDBRIDGE_DISCORD_TOKEN);
+};
+
+
+exports.onChatMessage = {
+    execute: (data: any, message: any) => {
+        //
+    }
+};
